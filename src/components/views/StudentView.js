0638fc3bd5a6f1735b1d8a42b20758ae33196299
TanmoyPaul1 <pault@bxscience.edu>
2023-05-08 19:16:46 -0400

    Finished DeleteStudent

--- src/components/views/StudentView.js
+++ src/components/views/StudentView.js
@@ -6,7 +6,7 @@ It constructs a React component to display the single student view page.
 ================================================== */
 import { Link } from "react-router-dom"
 const StudentView = (props) => {
-  const { student } = props;
+  const { student, deleteStudent } = props;
 
   // Render a single Student view 
   return (
@@ -15,7 +15,13 @@ const StudentView = (props) => {
       {student.imageUrl && <img src={student.imageUrl} alt={student.name} style={{width: 200, height: 100}}></img>}
       {student.email && <p>{student.email}</p>}
       {student.gpa && <p>GPA: {student.gpa}</p>}
-      {student.campus ? (<Link to={`/campus/${student.campus.id}`}><h3>{student.campus.name}</h3></Link>) : (<p>Student is in no campus</p>)}
+      {student.campus ? (<Link to={`/campus/${student.campus.id}`}><h3>{student.campus.name}</h3></Link>) : (<p style={{fontWeight: 'bold'}}>[STUDENT IS IN NO CAMPUS]</p>)}
+      <Link to={`/editstudents/${student.id}`}>
+        <button>Edit Student</button>
+      </Link>
+      <Link to={`/students`}>
+        <button onClick={() => deleteStudent(student.id)}>Delete Student</button>
+      </Link>
     </div>
   );
 
